bcd7487ec11c4e5ff44f31850bd0f04023bfada5
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-06-23 20:12:35 -0500

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,10 +16,10 @@ $(document)
 		$loading.hide();
 	});
 // Hamburger view
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-        
+$(document).ready(function () {
+	$('.sidenav').sidenav();
+});
+
 //Initiating drop down
 $(document).ready(function () {
 	var imageArray = ["assets/images/img-1.jpg", "assets/images/img-2.jpg", "assets/images/img-3.jpg", "assets/images/img-4.jpg", "assets/images/img-5.jpg", "assets/images/img-6.jpg", "assets/images/img-7.jpg", "assets/images/img-8.jpg", "assets/images/img-9.jpg", "assets/images/img-10.jpg"];
@@ -35,18 +35,10 @@ $(document).ready(function () {
 		$("#rental-results").empty();
 		$("#event-results").empty();
 
-	// Splitting words, replacing first letter of each word with capital letter and joining them so they can be added to url		
 
-		// var word1 = city[0].charAt(0).toUpperCase()+city[0].slice(1);
-		// if (city[1]==null){
-		// 	city=word1;
-		// }else if (city[1]){
-		// var word2 = city[1].charAt(0).toUpperCase()+city[1].slice(1);
-		// 	city = word1 + "%20" + word2
-		// }
-
+		// Splitting words, replacing first letter of each word with capital letter and joining them so they can be added to url		
 		var city = $("#input").val().split(" ");
-		var concatCity =" ";
+		var concatCity = " ";
 		if (city.length > 1) {
 			city.forEach(element => {
 				concatCity = concatCity + element.charAt(0).toUpperCase() + element.slice(1) + " ";
@@ -75,7 +67,7 @@ $(document).ready(function () {
 			console.log(response);
 			showListing(response);
 		});
-	
+
 		//Ajax call to get local events
 		var settings = {
 			"url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?limit=10&location=" + city + "," + selectedState + "&start_date=" + eventStartDate + "&end_date=" + eventEndDate + "&sort_on=time_start",
@@ -96,7 +88,7 @@ $(document).ready(function () {
 	function showListing(response) {
 		for (var i = 0; i < response.length; i++) {
 			var rentals = response[i];
-			var card = `<div class="card">
+			var card = `<div class="card animate__animated animate__fadeInUpBig">
 					<div class="row">
 						<div class="card-image col s4">
 							<img id="img-1" class="responsive-img" src="${imageArray[i]}">
@@ -107,7 +99,7 @@ $(document).ready(function () {
 							<p><strong>Property Type</strong>: ${rentals.propertyType == null ? " " : rentals.propertyType}</p>
 							<p><strong>No of bedroom</strong>: ${rentals.bedrooms == null ? " " : rentals.bedrooms}</p>
 							<p><strong>No of bathroom</strong>: ${rentals.bathrooms == null ? " " : rentals.bathrooms}</p><br>
-							<button class="buttonViewMap" data-index="${escape(rentals.formattedAddress == null ? " " : rentals.formattedAddress)}">View Map</button>
+							<a class="buttonViewMap waves-effect waves-light blue btn" data-index="${escape(rentals.formattedAddress == null ? " " : rentals.formattedAddress)}">View Map</a>
 						</div>
 					</div>
 			</div>`
@@ -119,23 +111,23 @@ $(document).ready(function () {
 	$('body').on("click", '.buttonViewMap', function () {
 		var buttonIndex = $(this).data('index');
 		console.log(buttonIndex)
-	
 
-	//GeoCode Address
+
+		//GeoCode Address
 		var settings = {
 			"async": true,
 			"crossDomain": true,
-			"url": "https://www.mapquestapi.com/geocoding/v1/address?key=rS5lDoNcX2uDA4T332RbG7npjFiUZ84p&location=" + buttonIndex ,
+			"url": "https://www.mapquestapi.com/geocoding/v1/address?key=rS5lDoNcX2uDA4T332RbG7npjFiUZ84p&location=" + buttonIndex,
 			"method": "GET",
 		}
 		$.ajax(settings).done(function (response) {
-		lat = response.results[0].locations[0].latLng.lat
-		lng = response.results[0].locations[0].latLng.lng
+			lat = response.results[0].locations[0].latLng.lat
+			lng = response.results[0].locations[0].latLng.lng
 
-		url= "https://www.mapquestapi.com/staticmap/v5/map?locations="+lat+ "," +lng+"&size=1280,800@2x&key=rS5lDoNcX2uDA4T332RbG7npjFiUZ84p"
-		// console.log(url)	
-		window.open(url);
-		});		
+			url = "https://www.mapquestapi.com/staticmap/v5/map?locations=" + lat + "," + lng + "&size=1280,800@2x&key=rS5lDoNcX2uDA4T332RbG7npjFiUZ84p"
+			// console.log(url)	
+			window.open(url);
+		});
 	});
 
 
@@ -144,7 +136,7 @@ $(document).ready(function () {
 
 		for (var i = 0; i < response.events.length; i++) {
 			var event = response.events[i];
-			var card = `<div class="card">
+			var card = `<div class="card animate__animated animate__fadeInUpBig">
 				<div class="row">
 					<div class="card-image col s4">
 						<img id="img-1" class="responsive-img" src="${event.image_url}">
@@ -152,8 +144,8 @@ $(document).ready(function () {
 					<div class="card-content col s8" id="content-1">
 						<h6><strong> Title: ${event.name}</strong></h6>
 						<p><strong>Description</strong>: ${event.description}</p>
-						<p><strong>Venue</strong>: ${event.location.display_address[0]}</p>
-						<button class="buttonReadMore"><a href="${event.event_site_url}" target ="_blank">Read More</a></button>
+						<p><strong>Venue</strong>: ${event.location.display_address[0]}</p><br>
+						<a class="buttonReadMore waves-effect waves-light blue btn" href="${event.event_site_url}" target ="_blank">Read More</a>
 					</div>
 				<div>	
 			</div>`
